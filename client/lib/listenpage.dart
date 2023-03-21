5a3c44681069c6b657da830405c8c1f05311a189
Monsieur Charcuter <hhdforwork@gmail.com>
2023-03-21 13:51:09 +0700

    play button and slider worked!!, timeset worked also

--- client/lib/listenpage.dart
+++ client/lib/listenpage.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
@@ -10,12 +9,37 @@ class listenpage extends StatefulWidget {
 }
 
 class _listenpageState extends State<listenpage> {
-  final shadow = BoxShadow(color: Colors.black54, blurRadius: 30);
   final audioPlayer = AudioPlayer();
   static bool isPlaying = false;
   static Duration duration = Duration.zero;
   static Duration position = Duration.zero;
-  static String musicPath = 'assets/Maroon-5-She-Will-Be-Loved.mp3';
+  static Duration seek = Duration.zero;
+  static String musicPath = './assets/Maroon-5-She-Will-Be-Loved.mp3';
+
+  @override
+  void initState() {
+    super.initState();
+    // TODO: implement initState
+    //xem state// pause, play, stop
+    audioPlayer.onPlayerStateChanged.listen((state) {
+      setState(() {
+        isPlaying = state == PlayerState.playing;
+      });
+    });
+
+    audioPlayer.onPositionChanged.listen((newPosition) {
+      setState(() {
+        position = newPosition;
+      });
+    });
+
+    audioPlayer.onDurationChanged.listen((newDuration) {
+      setState(() {
+        duration = newDuration;
+      });
+    });
+  }
+
   @override
   void dispose() {
     audioPlayer.dispose();
@@ -23,31 +47,30 @@ class _listenpageState extends State<listenpage> {
   }
 
   @override
-  void initState() {
-    // TODO: implement initState
-    audioPlayer.onPlayerStateChanged.listen((state) {
-      setState(() {
-        isPlaying = State == PlayerState.playing;
-      });
-    });
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final shadow = BoxShadow(
+        offset: Offset(15, 30),
+        // color: Color.fromARGB(255, 80, 40, 255).withOpacity(0.45),
+        color: Colors.black.withOpacity(0.9),
+        blurRadius: 50);
+
     final sliderProgress = Slider(
       min: 0,
       max: duration.inSeconds.toDouble(),
       value: position.inSeconds.toDouble(),
       activeColor: Color.fromRGBO(19, 41, 128, 1),
       thumbColor: Colors.deepPurpleAccent,
-      onChanged: (value) async {},
+      onChanged: (value) async {
+        position = Duration(seconds: value.toInt());
+        await audioPlayer.seek(Duration(seconds: value.toInt()));
+      },
     );
+
     String formatTime(Duration duration) {
       String twoDigits(int n) => n.toString().padLeft(2, '0');
-      final hours = twoDigits(duration.inHours);
-      final minutes = twoDigits(duration.inMinutes);
-      final seconds = twoDigits(duration.inSeconds);
+      final hours = twoDigits(duration.inHours.remainder(60));
+      final minutes = twoDigits(duration.inMinutes.remainder(60));
+      final seconds = twoDigits(duration.inSeconds.remainder(60));
 
       return [
         if (duration.inHours > 0) hours,
@@ -57,7 +80,7 @@ class _listenpageState extends State<listenpage> {
     }
 
     final timePlay = Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
+      padding: const EdgeInsets.symmetric(horizontal: 15),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -66,26 +89,34 @@ class _listenpageState extends State<listenpage> {
         ],
       ),
     );
-    final playButton = CircleAvatar(
-        radius: 35,
-        child: IconButton(
-          icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
-          iconSize: 35,
-          onPressed: () async {
-            if (isPlaying) {
-              await audioPlayer.pause();
-              initState();
-            } else {
-              String url = "assets/Maroon-5-She-Will-Be-Loved.mp3";
-
-              await audioPlayer.play(UrlSource(url));
-            }
-          },
-        ));
+    final playButton = Container(
+        child: CircleAvatar(
+            radius: 30,
+            backgroundColor: Colors.deepPurpleAccent,
+            foregroundColor: Colors.white,
+            child: DecoratedBox(
+                decoration: BoxDecoration(boxShadow: [
+                  BoxShadow(
+                      spreadRadius: 0,
+                      blurRadius: 30,
+                      offset: Offset(0, 15),
+                      color: Colors.deepPurpleAccent.withOpacity(0.6))
+                ]),
+                child: IconButton(
+                  icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
+                  iconSize: 40,
+                  onPressed: () async {
+                    if (isPlaying) {
+                      await audioPlayer.pause();
+                    } else {
+                      await audioPlayer.play(DeviceFileSource(musicPath));
+                    }
+                  },
+                ))));
 
     final playListName = Center(
       child: Text(
-        'W  E  D  D  I  N  G     P  L  A  Y  L  I  S  T',
+        'W E D D I N G    P L A Y L I S T',
         style: TextStyle(
             fontSize: 10,
             color: Color.fromRGBO(62, 84, 172, 1),
@@ -99,13 +130,21 @@ class _listenpageState extends State<listenpage> {
     final borderedImageOfPlaylist = Center(
       child: Container(
           padding: EdgeInsets.all(10.0),
-          child: ClipRRect(
-              borderRadius: BorderRadius.circular(15.0),
-              child: Image(
-                height: 180.0,
-                image: AssetImage('assets/she-will-be-loved.jpeg'),
-                fit: BoxFit.fill,
-              ))),
+          child: DecoratedBox(
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                    spreadRadius: -3,
+                    blurRadius: 30,
+                    offset: Offset(0, 20),
+                    color: Colors.deepPurpleAccent.withOpacity(0.20))
+              ]),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15.0),
+                  child: Image(
+                    height: 180.0,
+                    image: AssetImage('assets/she-will-be-loved.jpeg'),
+                    fit: BoxFit.fill,
+                  )))),
     );
     final nameOfSong = Center(
       child: Text(
@@ -147,13 +186,16 @@ class _listenpageState extends State<listenpage> {
               ),
             ),
             decoration: BoxDecoration(
+              boxShadow: [shadow],
               color: Color.fromRGBO(238, 238, 238, 5),
               borderRadius: BorderRadius.circular(25.0),
             )));
 
     return MaterialApp(
         home: Scaffold(
-            backgroundColor: Color.fromRGBO(15, 3, 125, 0.612),
+            // backgroundColor: Color.fromRGBO(68, 54, 173, 0.98),
+
+            backgroundColor: Color.fromARGB(248, 42, 42, 131).withOpacity(0.9),
             body: Center(
               child: ListView(
                 shrinkWrap: true,
