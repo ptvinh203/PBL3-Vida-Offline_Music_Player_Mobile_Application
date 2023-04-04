b767ac1e11d793651bc8679a3305952d58ca632e
Phạm Thành Vinh <thanhvinh07032003@gmail.com>
2023-04-04 18:48:32 +0700

    delete

--- server/src/main/java/com/pbl3/musicapplication/model/repository/ArtistRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pbl3.musicapplication.model.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.pbl3.musicapplication.model.entity.Artist;
-
-public interface ArtistRepository extends CrudRepository<Artist, Integer> {
-    
-}
